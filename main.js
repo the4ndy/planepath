b699510a0d06cb1a1e58fa30ef3c52333a636422
thesheriff <thesheriff@devsvr.net>
2017-06-07 21:59:00 -0400

    working v0.0.0.0.1

---
+++ main.js
@@ -0,0 +1,105 @@
+const electron = require('electron')
+// Module to control application life.
+const app = electron.app
+// Module to create native browser window.
+const BrowserWindow = electron.BrowserWindow
+
+const path = require('path')
+const url = require('url')
+
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow
+
+function createWindow () {
+
+
+// TODO 
+// 
+// use https://www.npmjs.com/package/electron-settings to allow the user to choose monitor to open on, frameless or not, etc
+//
+
+
+//uses the screen module to open the app on the first widescreen monitor that is NOT your primary monitor (because thats where your game is presumably)
+const Screen = require('electron').screen
+var allDisplays = Screen.getAllDisplays()
+var notThisDisplay = Screen.getPrimaryDisplay().id
+var externalDisplay = null
+
+  for (var i in allDisplays) {
+      
+    if (allDisplays[i].id != notThisDisplay && allDisplays[i].workArea.width > allDisplays[i].workArea.height) {
+      console.log(allDisplays[i])
+      externalDisplay = allDisplays[i];
+      break;
+    }
+  }
+
+var sWidth = externalDisplay.workArea.width
+var sHeight = externalDisplay.workArea.height
+
+  // Create the browser window.
+
+if (externalDisplay){
+  mainWindow = new BrowserWindow({
+      width: sWidth, 
+      height: sHeight,
+      backgroundColor: '#212121',
+      x: externalDisplay.bounds.x,
+      y: externalDisplay.bounds.y,
+ 
+})
+} else {
+  mainWindow = new BrowserWindow({
+      width: sWidth, 
+      height: sHeight,
+      backgroundColor: '#212121',
+ 
+})
+}
+
+
+  // and load the index.html of the app.
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function () {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null
+  })
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
